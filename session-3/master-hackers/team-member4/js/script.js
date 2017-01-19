bba1d218fe18a6228b54ca7d061a052d6256fdaa
interface-github <interface-github@interfaceschool.com>
2017-01-18 20:14:05 -0600

    Update script.js

--- session-3/master-hackers/team-member4/js/script.js
+++ session-3/master-hackers/team-member4/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     
        $("img").fadeOut("slow");
     });
-    $("h1").click(function(){
+    $("header").click(function(){
     
        $("header").css( "background-color", "red");  
         
