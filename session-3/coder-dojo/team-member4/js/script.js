b687fd18363e2e0724ae4dda9c8e4e25b65eb947
interface-github <interface-github@interfaceschool.com>
2017-01-18 20:15:34 -0600

    Update script.js

--- session-3/coder-dojo/team-member4/js/script.js
+++ session-3/coder-dojo/team-member4/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("img,h1,marquee").fadeIn(5000);
   $("header").click(function(){
     $("header").css("background-color", "red");
+    $("button").click(function(){
   });
 });
 
