7e62b9cee73f4ded3dcba7695c0d6a355a639dca
interface-github <interface-github@interfaceschool.com>
2017-01-18 20:12:39 -0600

    Update script.js

--- session-3/bits-and-bytes/team-member2/js/script.js
+++ session-3/bits-and-bytes/team-member2/js/script.js
@@ -1,4 +1,7 @@
 // fade in h1 and photos
 $(document).ready(function(){
-  $("img", "h1").fadeIn(3000);
+  $("img, h1").fadeIn(3000);
+  $("header").click(function(){
+    $("header").css("background-color", "red");
+  });
 });
