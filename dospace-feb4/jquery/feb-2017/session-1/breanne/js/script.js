e6d4400fec1dbebde3550850f1bb95a2507c7470
interface-github <interface-github@interfaceschool.com>
2017-02-04 16:10:18 -0600

    Update script.js

--- dospace-feb4/jquery/feb-2017/session-1/breanne/js/script.js
+++ dospace-feb4/jquery/feb-2017/session-1/breanne/js/script.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
   $("button").click(function(){
   $("img").fadeIn(5000);  
   });
-  
+  $("header, footer").click(function(){
+  $("header, footer").css("background-color", "green");
+  });
 });
