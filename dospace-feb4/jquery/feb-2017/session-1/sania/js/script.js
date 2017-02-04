17545977634b121d4ded226b05031adef62a4cbc
interface-github <interface-github@interfaceschool.com>
2017-02-04 16:12:07 -0600

    Update script.js

--- dospace-feb4/jquery/feb-2017/session-1/sania/js/script.js
+++ dospace-feb4/jquery/feb-2017/session-1/sania/js/script.js
@@ -3,5 +3,8 @@ $("h1,p").fadeIn(5000);
 $("button").click(function(){
 $("img").fadeIn(5000);
 });
+  $("header,footer").click(function(){
+$("header,footer").css("background-color","green"
+
 
 });
