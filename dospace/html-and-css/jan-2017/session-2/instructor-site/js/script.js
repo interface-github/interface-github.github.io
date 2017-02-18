23e2ff3db2364c2f5a73c68dd4fc3deca2c241d9
interface-github <interface-github@interfaceschool.com>
2017-02-18 11:49:13 -0600

    Update script.js

--- dospace/html-and-css/jan-2017/session-2/instructor-site/js/script.js
+++ dospace/html-and-css/jan-2017/session-2/instructor-site/js/script.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
   $("button").click(function(){
     $("img").fadeOut(5000);
   });
+  $("header").click(function(){
+    $('header').css("background-color", "orange");
+   }); 
 });
