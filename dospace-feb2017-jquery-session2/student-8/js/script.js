16ff4343e38b121f7c39fa5f44141e3454f53e8c
interface-github <interface-github@interfaceschool.com>
2017-02-18 16:21:06 -0600

    Update script.js

--- dospace-feb2017-jquery-session2/student-8/js/script.js
+++ dospace-feb2017-jquery-session2/student-8/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $("h1").fadeIn();
 
-    $(".in").click(function(){
+    $("in").click(function(){
     $("img").fadeIn();
   });
-  $(".out").click(function(){
+  $("out").click(function(){
     $("img").fadeOut();
   });
  
