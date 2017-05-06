267770c7a45c0c2f3003e284bf1f98bfec63e290
interface-github <interface-github@interfaceschool.com>
2017-05-06 16:16:34 -0500

    Update script.js

--- dospace/may2017/session1/jquery/student-9/js/script.js
+++ dospace/may2017/session1/jquery/student-9/js/script.js
@@ -5,5 +5,8 @@ $(document).ready(function() {
   });
   $("header, footer").click(function(){
     $(this).css("background", "#5a2372", "color":"white");
+     });
+      $("img").click(function(){
+    $(this).css("border-radius", "100px");
   });
 });
