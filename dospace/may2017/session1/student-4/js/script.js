42488b492214b74b829996ab1e276a6a9e5998f5
interface-github <interface-github@interfaceschool.com>
2017-05-06 15:59:58 -0500

    Update script.js

--- dospace/may2017/session1/student-4/js/script.js
+++ dospace/may2017/session1/student-4/js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   $('h1, p, img').fadeIn('slow');
   $('button').click(function() {
     $('img').fadeToggle(2000);
-  });  
+  });
+  
+  $('header, footer').click(function() {
+    $(this).css('background-color', #7c1f78);
+  });
 });
   
