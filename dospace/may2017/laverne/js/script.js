04618a5d84de591bd5d878417f170f0114f4ffd2
interface-github <interface-github@interfaceschool.com>
2017-05-20 16:17:11 -0500

    Update script.js

--- dospace/may2017/laverne/js/script.js
+++ dospace/may2017/laverne/js/script.js
@@ -1,1 +1,9 @@
-test
+$(document).ready(function(){
+  
+$("p").click(function(){
+  $(this).css("color","blue");
+});
+ $("div").mouseenter(function(){
+  $(this).css("border-radius","100px");
+});
+});
