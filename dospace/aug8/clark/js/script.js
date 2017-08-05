c475f6bf0ca610025856cf105c1437c89127ccf2
interface-github <interface-github@interfaceschool.com>
2017-08-05 15:41:38 -0500

    Create script.js

---
+++ dospace/aug8/clark/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('h1').click(function(){
+    $('h1').css("color", "green");
+    });
+   $('img').click(function(){
+    $('img').css("border-radius" "160px");
+   });
+   $('button').click(function() {
+    $('img').fadeOut(1000);
+  )};
+});
