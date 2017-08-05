4bb44c0d286fb129dcf4a46740f185b09fe1f6fe
interface-github <interface-github@interfaceschool.com>
2017-08-05 15:41:05 -0500

    Create script.js

---
+++ dospace/aug8/dave/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+$('h1).click(function(){
+$('h1').css("color:" "green");
+});
+
+$('img').click(function() {
+$('img').css("border-radius", "160px");
+});
+
+$('button').click(function() {
+$('img').fadeout(1000);
+});
+});
