f6e4209e6f2a4b78ad2dd69bc2c2098f9098ffc8
interface-github <interface-github@interfaceschool.com>
2017-08-19 16:02:13 -0500

    Update script.js

--- dospace/aug19/jyothsna/js/script.js
+++ dospace/aug19/jyothsna/js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
  $('h1').fadeIn(2000); 
+ 
  $('button').click(function(){
   $('img').fadeOut(5000);
  });
+ 
+ $('header').click(function(){
+  $('header').css("background-color", "red");
+ });
+ 
+ $('p').click(function(){
+  $(this).css("font-color","red");
+ });
+ 
+ 
 });
