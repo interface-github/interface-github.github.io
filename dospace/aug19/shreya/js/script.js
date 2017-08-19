0784eb87cde20161c5489e35731fa6e105a8e0b6
interface-github <interface-github@interfaceschool.com>
2017-08-19 16:07:33 -0500

    Update script.js

--- dospace/aug19/shreya/js/script.js
+++ dospace/aug19/shreya/js/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
   $('h1').fadeIn(3000); 
+  
   $('button').click(function() {
     $('img').fadeOut(5000); 
   });
-  $('header').css('background-color', 'red');
-    $(this).css ('font-color','red');
   
+  $('header').click(function() {
+     $('header').css ("background-color","red");
+  
+  });
+  $ ('p').click(function(){
+    $(this).css("color","red"):
   });
   
 });
