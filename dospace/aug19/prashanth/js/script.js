d1dbe3c38a1c7d7fa3d68d1a713c5b5ca9fcdc26
interface-github <interface-github@interfaceschool.com>
2017-08-19 16:03:06 -0500

    Update script.js

--- dospace/aug19/prashanth/js/script.js
+++ dospace/aug19/prashanth/js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function (){
   $('h1').fadeIn(3000);
   $('button').click(function(){
     $('img').fadeOut(5000);
+    $('p').click(function(){
+    $(this).css("font-color","red");
   });
  });
 
