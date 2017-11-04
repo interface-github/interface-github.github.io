382964c42830f14a0e73eae901be2a10315d4330
interface-github <interface-github@interfaceschool.com>
2017-11-04 16:19:29 -0500

    Update script.js

--- dospace/jquery/nov-2017/shonna/js/script.js
+++ dospace/jquery/nov-2017/shonna/js/script.js
@@ -1,1 +1,9 @@
-script
+$(document).ready(function() {
+  $('img').fadeIn(5000);
+  $('button').click(function(){
+    $('img').fadeOut(2000);
+  });
+  $('header').click(function(){
+    $('header').css("background", "red");
+  });
+});
