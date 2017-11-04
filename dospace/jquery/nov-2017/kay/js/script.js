8b7ae1356fb4acb6d1b148125163d8c76085415f
interface-github <interface-github@interfaceschool.com>
2017-11-04 16:18:05 -0500

    Update script.js

--- dospace/jquery/nov-2017/kay/js/script.js
+++ dospace/jquery/nov-2017/kay/js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
   $('button').click(function(){
     $('img').fadeOut(2000);
   });
+  $('header').click(function(){
+    $('header').css("background","red");
+  });
+  
 });
