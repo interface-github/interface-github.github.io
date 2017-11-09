6181dc719abee54e13af5ff9a02cd63b8ecbe58a
interface-github <interface-github@interfaceschool.com>
2017-11-09 15:18:56 -0600

    Update script.js

--- dospace/jquery/nov-2017/sterling/js/script.js
+++ dospace/jquery/nov-2017/sterling/js/script.js
@@ -14,15 +14,25 @@ $(document).ready(function() {
 
 });
 
-<p><button> push me hard </button></p>
+//<p><button> push me hard </button></p>
 	
-<div class= "first"> </div>
-<div class= "second"> </div>
+//<div class= "first"> </div>
+//<div class= "second"> </div>
  
-<script>
-$( "button" ).click(function() {
-  $( "div.first" ).slideUp( 300 ).delay( 800 ).fadeIn( 400 );
-  $( "div.second" ).slideUp( 300 ).fadeIn( 400 );
+//<script>
+//$( "button" ).click(function() {
+  //$( "div.first" ).slideUp( 300 ).delay( 800 ).fadeIn( 400 );
+ // $( "div.second" ).slideUp( 300 ).fadeIn( 400 );
    });
 
+//});
+
+$(document).ready(function() {
+  $('img').fadeIn(5000);
+  $('button').click(function(){
+    $('img').fadeOut(2000);
+  });
+  $('header').click(function(){
+    $('header').css("background", "red");
+  });
 });
