a2cfaa338cbb5965cb1833f25676069af908e131
interface-github <interface-github@interfaceschool.com>
2018-07-07 15:15:10 -0500

    Update script.js

--- july7/rachel/js/script.js
+++ july7/rachel/js/script.js
@@ -3,4 +3,13 @@ $("#blue-box").fadeIn();
   $("button").click(function(){
     $("#blue-box").fadeOut();
   });
-});
+  
+  $("p").click(function)(){
+    $("p").css("color","lavender");
+  });
+  
+  $("#blue-box").click(function)(){
+    $("#blue-box").css("background-color","aqua");
+  });
+
+
