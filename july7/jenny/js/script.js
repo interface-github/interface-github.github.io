3ac17ac7842d94edfaf796377531c58539ce31db
interface-github <interface-github@interfaceschool.com>
2018-07-07 15:16:54 -0500

    Update script.js

--- july7/jenny/js/script.js
+++ july7/jenny/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $("#blue-box").fadeIn(5000);
+  $("#blue-box").fadeIn(20000);
   $("button").click (function(){ 
-  $("#blue-box").fadeOut();
+  $("#blue-box").fadeOut(20000);
   });
   
   $("p").click(function(){
